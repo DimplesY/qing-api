870962625663cccff05ee57092565346e1a7b114
Dimples_YJ <2890841438@qq.com>
2023-02-12 20:56:07 +0800

    feat: 增加文章顶部tab

---
+++ src/api/article-tab/controllers/article-tab.ts
@@ -0,0 +1,7 @@
+/**
+ * article-tab controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::article-tab.article-tab');
---
+++ src/api/article-tab/routes/article-tab.ts
@@ -0,0 +1,7 @@
+/**
+ * article-tab router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::article-tab.article-tab');
